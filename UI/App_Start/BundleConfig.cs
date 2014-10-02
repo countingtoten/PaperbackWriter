4145c1a92d903f043bd4334b4d737226cbbf52a5
Jim <countingtoten@gmail.com>
2014-10-02 14:30:36 -0500

    Add Web.Optimization to bundle the two files needed for RequireJS

---
+++ UI/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace UI
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/requirejs").Include(
+                        "~/Scripts/require.js",
+                        "~/Scripts/common.js"));
+        }
+    }
+}
